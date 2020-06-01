e774c5d41df7d2683a060d051a784767f368974e
Jules Vesnat <59823755+vj703676@users.noreply.github.com>
2020-06-01 09:31:18 +0200

    Update Scene1.js

--- Scene1.js
+++ Scene1.js
@@ -11,7 +11,7 @@ class Scene1 extends Phaser.Scene {
         this.load.image('ground', 'PI-jeux-educatifs-Jules-Vesnat/assets/ground.png');
         this.load.image('button', 'PI-jeux-educatifs-Jules-Vesnat/assets/button2.png');
         this.load.image('guard', 'PI-jeux-educatifs-Jules-Vesnat/assets/guard.png');
-        this.load.image('menu1', 'PI-jeux-educatifs-Jules-Vesnat/assets/Menu1.JPG');
+        this.load.image('menu1', 'PI-jeux-educatifs-Jules-Vesnat/assets/Menu1.jpg');
         this.load.spritesheet('1', 'PI-jeux-educatifs-Jules-Vesnat/assets/1.png', { frameWidth: 203, frameHeight: 173 });
         this.load.spritesheet('2', 'PI-jeux-educatifs-Jules-Vesnat/assets/2.png', { frameWidth: 203, frameHeight: 175 });
         this.load.spritesheet('3', 'PI-jeux-educatifs-Jules-Vesnat/assets/3.png', { frameWidth: 167, frameHeight: 158 });
