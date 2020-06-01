f6529d6e1e84586fa556e10d128da671896ceb52
Jules Vesnat <59823755+vj703676@users.noreply.github.com>
2020-06-01 09:29:05 +0200

    Update Scene1.js

--- Scene1.js
+++ Scene1.js
@@ -7,17 +7,17 @@ class Scene1 extends Phaser.Scene {
     
     preload(){
         //this.load.video('menu', 'assets/video.mp4', 'loadeddata', false, true);
-        this.load.image('back', 'assets/florence.png');
-        this.load.image('ground', 'assets/ground.png');
-        this.load.image('button', 'assets/button2.png');
-        this.load.image('guard', 'assets/guard.png');
-        this.load.image('menu1', 'assets/Menu1.Jpg');
-        this.load.spritesheet('1', 'assets/1.png', { frameWidth: 203, frameHeight: 173 });
-        this.load.spritesheet('2', 'assets/2.png', { frameWidth: 203, frameHeight: 175 });
-        this.load.spritesheet('3', 'assets/3.png', { frameWidth: 167, frameHeight: 158 });
-        this.load.audio('music', 'assets/music.mp3');
-        this.load.audio('sound1', 'assets/town.ogg');
-        this.load.audio('sound3', 'assets/italianmusic.mp3');
+        this.load.image('back', 'PI-jeux-educatifs-Jules-Vesnat/assets/florence.png');
+        this.load.image('ground', 'PI-jeux-educatifs-Jules-Vesnat/assets/ground.png');
+        this.load.image('button', 'PI-jeux-educatifs-Jules-Vesnat/assets/button2.png');
+        this.load.image('guard', 'PI-jeux-educatifs-Jules-Vesnat/assets/guard.png');
+        this.load.image('menu1', 'PI-jeux-educatifs-Jules-Vesnat/assets/Menu1.Jpg');
+        this.load.spritesheet('1', 'PI-jeux-educatifs-Jules-Vesnat/assets/1.png', { frameWidth: 203, frameHeight: 173 });
+        this.load.spritesheet('2', 'PI-jeux-educatifs-Jules-Vesnat/assets/2.png', { frameWidth: 203, frameHeight: 175 });
+        this.load.spritesheet('3', 'PI-jeux-educatifs-Jules-Vesnat/assets/3.png', { frameWidth: 167, frameHeight: 158 });
+        this.load.audio('music', 'PI-jeux-educatifs-Jules-Vesnat/assets/music.mp3');
+        this.load.audio('sound1', 'PI-jeux-educatifs-Jules-Vesnat/assets/town.ogg');
+        this.load.audio('sound3', 'PI-jeux-educatifs-Jules-Vesnat/assets/italianmusic.mp3');
     
     }
 
